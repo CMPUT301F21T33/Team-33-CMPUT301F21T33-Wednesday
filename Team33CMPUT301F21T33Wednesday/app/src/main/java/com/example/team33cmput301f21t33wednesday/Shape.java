4e0a9699da0ae3240f00acab740f6ced71c241e0
evandermeer84 <evanderm@ualberta.ca>
2021-09-29 17:29:50 -0600

    New String

--- Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Shape.java
+++ Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Shape.java
@@ -3,9 +3,11 @@ package com.example.team33cmput301f21t33wednesday;
 public class Shape {
     private int x;
     private int y;
+    private String colour;
 
-    public void Shape (int x, int y){
+    public void Shape (int x, int y, String color){
         this.x = x;
         this.y = y;
+        this.colour = color;
     }
 }
