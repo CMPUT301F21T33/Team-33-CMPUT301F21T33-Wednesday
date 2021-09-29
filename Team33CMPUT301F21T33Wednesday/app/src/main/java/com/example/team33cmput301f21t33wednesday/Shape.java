158fcc8ba1a7e327e18fba1cccd4809c99817f76
evandermeer84 <evanderm@ualberta.ca>
2021-09-29 17:31:32 -0600

    New Colour

--- Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Shape.java
+++ Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Shape.java
@@ -3,9 +3,11 @@ package com.example.team33cmput301f21t33wednesday;
 public class Shape {
     private int x;
     private int y;
+    private String colour;
 
     public void Shape (int x, int y){
         this.x = x;
         this.y = y;
+        this.colour = "Red";
     }
 }
