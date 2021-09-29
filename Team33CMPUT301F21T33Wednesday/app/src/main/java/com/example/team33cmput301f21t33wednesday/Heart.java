d6daac4655a3204e2f3752fcdba524e906950a25
evandermeer84 <evanderm@ualberta.ca>
2021-09-29 17:22:38 -0600

    New File

---
+++ Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Heart.java
@@ -0,0 +1,13 @@
+package com.example.team33cmput301f21t33wednesday;
+
+public class Heart extends Shape{
+    private int loveAmount;
+    private int heartSize;
+    private String colour;
+
+    public void Heart(int loveAmount, int heartSize, String colour){
+        this.loveAmount = loveAmount;
+        this.heartSize = heartSize;
+        this.colour = colour;
+    }
+}
--- Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Shape.java
+++ Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Shape.java
@@ -3,4 +3,9 @@ package com.example.team33cmput301f21t33wednesday;
 public class Shape {
     private int x;
     private int y;
+
+    public void Shape (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
