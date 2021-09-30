5e8e293abc54bc36737777a5cb12f0467b18f4b6
Sam <sam.r.okeefe@gmail.com>
2021-09-29 18:26:53 -0600

    new file

--- Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Triangle.java
+++ Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Triangle.java
@@ -1,2 +1,15 @@
-package com.example.team33cmput301f21t33wednesday;public class Triangle {
+package com.example.team33cmput301f21t33wednesday;
+
+public class Triangle extends Shape{
+    private int name;
+    private int height;
+    private String colour;
+
+
+    @Override
+    public void Shape(int name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.colour = colour;
+    }
 }
