fc49fc880030eb8e95f8627b390765a9df633c63
evandermeer84 <evanderm@ualberta.ca>
2021-09-29 18:36:18 -0600

    sdkjhs

--- Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Shape.java
+++ Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Shape.java
@@ -3,7 +3,7 @@ package com.example.team33cmput301f21t33wednesday;
 public class Shape {
     private int x;
     private int y;
-    private String colour;
+    private String color;
 
     public void Shape (int x, int y){
         this.x = x;
