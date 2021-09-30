68e053a4587e26163fe73983b780d4ce7895f281
Balreet Grewal <balreet@ualberta.ca>
2021-09-29 18:03:05 -0600

    Added hexagons for bees

---
+++ Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/hexagon.java
@@ -0,0 +1,13 @@
+package com.example.team33cmput301f21t33wednesday;
+
+public class hexagon extends Shape{
+	private int sidelength;
+	private int numBees;
+	private String colour;
+
+	public void hexagon(int sidelength, int numBees, String colour) {
+		this.sidelength = sidelength;
+		this.numBees = numBees;
+		this.colour = colour;
+	}
+}
