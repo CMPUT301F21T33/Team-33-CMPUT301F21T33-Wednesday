429f37d3c24eefe0bc9769dd466dfabead47af3f
evandermeer84 <60145393+evandermeer84@users.noreply.github.com>
2021-09-29 18:35:19 -0600

    Update Shape.java

--- Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Shape.java
+++ Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Shape.java
@@ -3,7 +3,7 @@ package com.example.team33cmput301f21t33wednesday;
 public class Shape {
     private int x;
     private int y;
-    private String colour;
+    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
     public void Shape (int x, int y, String color){
         this.x = x;
