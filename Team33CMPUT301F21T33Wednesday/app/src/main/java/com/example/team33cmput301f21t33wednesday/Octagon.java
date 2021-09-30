91674cf06db39ce9992e9a7ee7fb7c3811cb8783
matt sasakamoose <matt.sasakamoose@gmail.com>
2021-09-29 19:13:35 -0600

    Fixed mess up

---
+++ Team33CMPUT301F21T33Wednesday/app/src/main/java/com/example/team33cmput301f21t33wednesday/Octagon.java
@@ -0,0 +1,9 @@
+package com.example.team33cmput301f21t33wednesday;
+
+public class Octagon extends Shape {
+    private int points;
+
+    public Octagon(int points) {
+        this.points = points;
+    }
+}
